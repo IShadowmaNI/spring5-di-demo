1da244453b3ff7d63ff208b471578fb78e548d6a
IShadowmaNI <marian.agheorghiesei@gmail.com>
2022-08-27 23:36:12 +0300

    Adding service examples.

--- src/main/java/guru/springframework/Spring5DiDemoApplication.java
+++ src/main/java/guru/springframework/Spring5DiDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5DiDemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Spring5DiDemoApplication.class, args);
     }
 
